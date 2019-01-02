037ed27aaef5c2915be8e40f6475b265ac8a5008
Anthony White <brancoantonio18@gmail.com>
2019-01-02 15:33:39 +0000

    Fixed for master

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 // ----- Package Imports ----- //
 import express from 'express';
-import compression from 'compression'
+import compression from 'compression';
 import chalk from 'chalk';
 // ----- File Imports ----- //
 import tools from './tools';
@@ -12,20 +12,26 @@ const app = express();
 
 // mongoose.connect("mongodb://srv-captain--mongo-db/mydatabase", { useMongoClient: true });
 // <-------------- compression --------------> //
-// app.use(compression())
+app.use(compression());
 
-
-app.use(express.static(tools.comingSoonPath()), (req, res, next) => {
-  next()
-});
+app.use(
+  express.static(tools.comingSoonPath()),
+  (req, res, next) => {
+    next();
+  },
+);
 
 // app.use(express.static(tools.publicPath()));
 
 app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT!!!!!' });
+  res.send({
+    express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT!!!!!',
+  });
   console.log('Connection successful!');
 });
 
 app.listen(port, () => {
-  console.log(chalk.white.bgRed(`Server started on port ${port} ...`));
+  console.log(
+    chalk.white.bgRed(`Server started on port ${port} ...`),
+  );
 });
